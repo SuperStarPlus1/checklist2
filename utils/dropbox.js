406f4a26ca9dcf53af336c30d8bb02f178be7207
SuperStarPlus1 <Ashraf@superstar-plus.co.il>
2025-06-27 19:40:44 +0300

    Create dropbox.js

---
+++ utils/dropbox.js
@@ -0,0 +1,27 @@
+// utils/dropbox.js
+export async function getDropboxAccessToken() {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'refresh_token');
+  params.append('refresh_token', process.env.DROPBOX_REFRESH_TOKEN);
+
+  const res = await fetch('https://api.dropboxapi.com/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(
+        `${process.env.DROPBOX_APP_KEY}:${process.env.DROPBOX_APP_SECRET}`
+      ).toString('base64'),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: params
+  });
+
+  if (!res.ok) {
+    const error = await res.text();
+    console.error('Failed to refresh token:', error);
+    throw new Error('Cannot refresh Dropbox token');
+  }
+
+  const data = await res.json();
+  return data.access_token;
+}
+  
